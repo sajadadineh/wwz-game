d31db427ba64fc8d8bce987acd9decb9a09ab3ce
sajadadineh <sajadadineh@protonmail.com>
2024-03-11 02:51:26 +0330

    init structs

--- main.go
+++ main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"github.com/nsf/termbox-go"
 )
 
+type Location struct {
+	x int
+	y int
+}
+
+type Bullet struct {
+	location Location
+}
+
+type Player struct {
+	symbol   rune
+	location Location
+	died     bool
+}
+
+type Zambi struct {
+	symbol   rune
+	location Location
+	died     bool
+}
+
+type World struct {
+	player  Player
+	height  int
+	width   int
+	bullets []Bullet
+	zambi   []Zambi
+}
+
 func main() {
-	fmt.Println("hello world")
+	maxX, maxY := termbox.Size()
+	fmt.Println(maxX, maxY)
 }
